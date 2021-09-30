14d71e10e9a398e7815a74e19d185c5c41ef24da
mpprasanthi <mpprasanthi@gmail.com>
2021-09-30 15:08:02 -0500

    commited

--- src/com/example/bank_app/BankAccount.java
+++ src/com/example/bank_app/BankAccount.java
@@ -5,14 +5,37 @@ public class BankAccount {
     double savingsBalance = 0;
     final double checkingOpeningDeposit;
     final double savingsOpeningDeposit;
+    double newCheckingBalance;
 
     public BankAccount(double checkingOpeningDeposit, double savingsOpeningDeposit) {
         this.checkingOpeningDeposit = checkingOpeningDeposit;
         this.savingsOpeningDeposit = savingsOpeningDeposit;
         checkingBalance = checkingBalance + checkingOpeningDeposit;
+        savingsBalance = savingsBalance + savingsOpeningDeposit;
     }
     public double getCheckingBalance() {
-        System.out.println("Balance is :" + checkingBalance);
+        System.out.println("Checking Balance is :" + checkingBalance);
         return 0;
     }
+    public double getSavingsBalance() {
+        System.out.println("Savings Balance is :" + savingsBalance);
+        return 0;
+    }
+    public void withDrawals(double withdrawalAmt) {
+        if(withdrawalAmt > checkingBalance){
+            System.out.println("Withdraw amount is greater than Checking Balance");
+            return;
+        }
+        newCheckingBalance = checkingBalance - withdrawalAmt;
+        if(newCheckingBalance <= 0){
+            System.out.println("Insufficient Balance to withdraw");
+        }else {
+            System.out.println("New Balance is :" + newCheckingBalance);
+        }
+    }
+    public void depositAmt(double depAmt) {
+        newCheckingBalance = newCheckingBalance + depAmt;
+        System.out.println("Checking Balance after deposit is:"+ newCheckingBalance);
+    }
+
 }
--- src/com/example/bank_app/BankApp.java
+++ src/com/example/bank_app/BankApp.java
@@ -5,5 +5,8 @@ public class BankApp {
     public static void main(String[] args){
     BankAccount customerAcct = new BankAccount(100,200);
     customerAcct.getCheckingBalance();
+    customerAcct.getSavingsBalance();
+    customerAcct.withDrawals(50);
+    customerAcct.depositAmt(100);
     }
 }
